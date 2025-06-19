d3ec2b39974cbda081fb5972f04a76f18e5d4c64
chiamaka-igbokwe <chiamakaigbokwex@gmail.com>
2025-06-19 11:51:49 +0100

    new commit

--- SafeSpace/src/App.tsx
+++ SafeSpace/src/App.tsx
@@ -1,19 +1,36 @@
+import Chats from "./components/pages/Chats";
+import Diet from "./components/pages/Diet";
+import Exercise from "./components/pages/Exercise";
+import Home from "./components/pages/Home";
+import Motivation from "./components/pages/Motivation";
+import Search from "./components/pages/Search";
 
-import Chats from "./components/pages/Chats"
-import Diet from "./components/pages/Diet"
-import Exercise from "./components/pages/Exercise"
-import Motivation from "./components/pages/Motivation"
-import Search from "./components/pages/Search"
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
 function App() {
   return (
-      <div className="bg-red-500">
-    <Chats />
-    <Search />
-    <Diet />
-    <Exercise />
-    <Motivation />
+    <BrowserRouter>
+      <nav className="space-x-4 p-4 bg-gray-100">
+        <Link to="/home">Home</Link>
+        <Link to="/chats">Chats</Link>
+        <Link to="/motivation">Motivation</Link>
+        <Link to="/diet">Diet</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/exercise">Exercise</Link>
+      </nav>
+
+      <div className="p-4">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/diet" element={<Diet />} />
+          <Route path="/motivation" element={<Motivation />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/exercise" element={<Exercise />} />
+        </Routes>
       </div>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ SafeSpace/src/components/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='flex flex-col h-screen bg-black'>
+      <h1 className='bg-amber-400 min-h-[200px] p-2 text-center px-50 mx-50'>Chat me</h1>
+    </div>
+  )
+}
+
+export default Home
